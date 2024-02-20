93341cf2bd4f97931204c742622516f3ea6d6d6f
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-20 13:14:36 +0100

    Update crs_controller.groovy

--- crs_controller.groovy
+++ crs_controller.groovy
@@ -4,9 +4,7 @@ pipeline {
         LOGS_PATH = "./Code"
         ARTIFACTS_DOWNLOAD_PATH = "C:/Users/${env.GITLAB_USER_LOGIN}/Downloads"
     }
-
     stages {
-
         stage('testing') {
             agent {
                 label 'EC2MatlabServer'
@@ -17,47 +15,12 @@ pipeline {
                         matlabScript("crs_controllerTestFile;")
                     }
                 }
-                post {
-                    always {
-                        //archiveArtifacts(artifacts: ["./Design/crs_controller/pipeline/analyze/**/*", "$LOGS_PATH/logs/", "./Code/codegen/crs_controller_ert_rtw"])
-                        junit './Design/crs_controller/pipeline/analyze/testing/crs_controllerJUnitFormatTestResults.xml'
-                    }
-                }
             }
         }
-
-        stage('package') {
-            agent {
-                label 'EC2MatlabServer'
-            }
-            steps {
-                script {
-                    ignoringErrorsAndWarnings {
-                        echo "The model crs_controller has been checked"
-                        echo "There is a Summary report generated crs_controllerReport.html"
-                        //matlabScript("generateXMLFromLogs('crs_controller'); generateHTMLReport('crs_controller'); deleteLogs;")
-                    }
-                }
-            }
-        }
-
-        stage('Deploy') {
-            agent {
-                label 'EC2MatlabServer'
-            }
-            steps {
-                script {
-                    ignoringErrorsAndWarnings {
-                        echo "Any deployments of code can be made here"
-                        echo "All artifacts of previous stage can be found here"
-                    }
-                }
-            }
-            post {
-                always {
-                    archiveArtifacts(artifacts: ["./Design/crs_controller/pipeline/analyze/**/*", "./Code/codegen/crs_controller_ert_rtw"])
-                }
-            }
+    }
+    post {
+        always {
+            junit './Design/crs_controller/pipeline/analyze/testing/crs_controllerJUnitFormatTestResults.xml'
         }
     }
 }
