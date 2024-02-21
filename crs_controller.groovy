734703d52c37783747214aa79f210ecfc000688b
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-21 09:13:18 +0100

    Update crs_controller.groovy

--- crs_controller.groovy
+++ crs_controller.groovy
@@ -1,37 +1,103 @@
 pipeline {
-    agent {
-    label 'EC2MatlabServer'
-}
+    agent none
     environment {
-        LOGS_PATH = "./Code"
+        LOGS_PATH = "Code"
         ARTIFACTS_DOWNLOAD_PATH = "C:/Users/${env.GITLAB_USER_LOGIN}/Downloads"
     }
     stages {
-        stage('testing') {
+        stage('Verify') {
+            agent {
+                label 'EC2MatlabServer' // Label for Windows agent
+            }
             steps {
                 script {
-                    node('EC2MatlabServer') {
-                        ignoringErrorsAndWarnings {
-                            matlabScript("crs_controllerTestFile;")
-                        }
-                    }
+                    // This job executes the Model Advisor Check for the model
+                    matlabScript("crs_controllerModelAdvisor;")
+                }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "$LOGS_PATH/logs/, ./Design/crs_controller/pipeline/analyze/**/*"
                 }
             }
         }
-    }  
-}
 
-def ignoringErrorsAndWarnings(body) {
-    try {
-        body()
-    } catch (e) {
-        if (!(e.toString().contains("LicenseManager") && e.toString().contains("checkout failed"))) {
-            echo "Error message: ${e}"
-            currentBuild.result = 'FAILURE'
+        stage('Build') {
+            agent {
+                label 'LocalMatlabServer' // Label for Windows agent
+            }
+            steps {
+                script {
+                    // This job performs code generation on the model
+                    matlabScript("crs_controllerBuild;")
+                }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "./Code/codegen/crs_controller_ert_rtw, ./Design/crs_controller/pipeline/analyze/**/*, $LOGS_PATH/logs/"
+                }
+            }
+        }
+
+        stage('Testing') {
+            agent {
+                label 'EC2MatlabServer' // Label for EC2 agent
+            }
+            steps {
+                script {
+                    // This job executes the unit tests defined in the collection
+                    matlabScript("crs_controllerTestFile;")
+                }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "./Design/crs_controller/pipeline/analyze/**/*, $LOGS_PATH/logs/, ./Code/codegen/crs_controller_ert_rtw"
+                    junit './Design/crs_controller/pipeline/analyze/testing/crs_controllerJUnitFormatTestResults.xml'
+                }
+            }
+        }
+
+        stage('Package') {
+            agent {
+                label 'LocalMatlabServer' // Label for Windows agent
+            }
+            steps {
+                script {
+                    // The summary report is generated which shows results from the previous stages.
+                    // Any logs that were generated in the previous stages will be cleared after this stage
+                    echo "The model CruiseControlMode has been checked"
+                    echo "There is a Summary report generated crs_controllerReport.html"
+                    matlabScript("generateXMLFromLogs('crs_controller'); generateHTMLReport('crs_controller'); deleteLogs;")
+                }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "./Design/crs_controller/pipeline/analyze/**/*, ./Code/codegen/crs_controller_ert_rtw"
+                }
+            }
+        }
+
+        stage('Deploy') {
+            agent {
+                label 'WinLocalagent' // Label for Windows agent
+            }
+            steps {
+                echo "Any deployments of code can be made here"
+                echo "All artifacts of previous stage can be found here"
+                script {
+                    // Curl command to download artifacts
+                    bat "curl.exe --location --output \"$ARTIFACTS_DOWNLOAD_PATH/Crs_ControllerArtifacts.zip\" --header \"PRIVATE-TOKEN: %CIPROJECTTOKEN%\" \"%CI_SERVER_URL%/api/v4/projects/%CI_PROJECT_ID%/jobs/artifacts/%CI_COMMIT_BRANCH%/download?job=Crs_ControllerPackage\""
+                }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "./Design/crs_controller/pipeline/analyze/**/*, ./Code/codegen/crs_controller_ert_rtw"
+                }
+            }
         }
     }
 }
 
 def matlabScript(String script) {
-    bat "matlab -batch \"openProject('CruiseControlSystem.prj'); ${script}\""
+    bat "matlab -nodesktop -batch \"openProject('CruiseControlSystem.prj'); ${script}\""
 }
