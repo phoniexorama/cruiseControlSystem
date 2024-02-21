0959708acf32f15a5ad832e7ac41d37ac759f09f
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-21 09:29:24 +0100

    Update driverSwRequest.groovy

--- driverSwRequest.groovy
+++ driverSwRequest.groovy
@@ -1,58 +1,66 @@
 pipeline {
-    agent {
-        label 'WinLocalagent' //Label for Windows agent
-    }
-
+    agent none
     environment {
-        LOGS_PATH = "./Code"
+        LOGS_PATH = "Code"
         ARTIFACTS_DOWNLOAD_PATH = "C:/Users/${env.GITLAB_USER_LOGIN}/Downloads"
     }
-
     stages {
         stage('Verify') {
+            agent {
+                label 'EC2MatlabServer' // Label for Windows agent
+            }
             steps {
                 script {
-                    // This job runs the Model Advisor Check file for DriverSwRequest models
+                    // This job executes the Model Advisor Check for the model
                     matlabScript("DriverSwRequestModelAdvisor;")
                 }
-                post {
-                    always {
-                        archiveArtifacts(artifacts: ["$LOGS_PATH/logs/", "./Design/DriverSwRequest/pipeline/analyze/**/*"])
-                    }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "$LOGS_PATH/logs/, ./Design/DriverSwRequest/pipeline/analyze/**/*"
                 }
             }
-        } 
+        }
 
         stage('Build') {
+            agent {
+                label 'LocalMatlabServer' // Label for Windows agent
+            }
             steps {
                 script {
                     // This job performs code generation on the model
                     matlabScript("DriverSwRequestBuild;")
                 }
-                post {
-                    always {
-                        archiveArtifacts(artifacts: ["./Code/codegen/DriverSwRequest_ert_rtw", "./Design/DriverSwRequest/pipeline/analyze/**/*", "$LOGS_PATH/logs/"])
-                    }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "./Code/codegen/DriverSwRequest_ert_rtw, ./Design/DriverSwRequest/pipeline/analyze/**/*, $LOGS_PATH/logs/"
                 }
             }
         }
 
         stage('Testing') {
+            agent {
+                label 'EC2MatlabServer' // Label for EC2 agent
+            }
             steps {
                 script {
-                    // This job runs the unit tests defined in the collection
+                    // This job executes the unit tests defined in the collection
                     matlabScript("DriverSwRequestTest;")
                 }
-                post {
-                    always {
-                        archiveArtifacts(artifacts: ["./Design/DriverSwRequest/pipeline/analyze/**/*", "$LOGS_PATH/logs/", "./Code/codegen/DriverSwRequest_ert_rtw"])
-                        junit './Design/DriverSwRequest/pipeline/analyze/testing/DriverSwRequestJUnitFormatTestResults.xml'
-                    }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "./Design/DriverSwRequestTest/pipeline/analyze/**/*, $LOGS_PATH/logs/, ./Code/codegen/DriverSwRequestTest_ert_rtw"
+                    junit './Design/DriverSwRequestTest/pipeline/analyze/testing/DriverSwRequestTestJUnitFormatTestResults.xml'
                 }
             }
         }
 
         stage('Package') {
+            agent {
+                label 'LocalMatlabServer' // Label for Windows agent
+            }
             steps {
                 script {
                     // The summary report is generated which shows results from the previous stages.
@@ -61,15 +69,18 @@ pipeline {
                     echo "There is a Summary report generated DriverSwRequestSummaryReport.html which is present in analyze folder"
                     matlabScript("generateXMLFromLogs('DriverSwRequest'); generateHTMLReport('DriverSwRequest'); deleteLogs;")
                 }
-                post {
-                    always {
-                        archiveArtifacts(artifacts: ["./Design/DriverSwRequest/pipeline/analyze/**/*", "./Code/codegen/DriverSwRequest_ert_rtw"])
-                    }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "./Design/DriverSwRequest/pipeline/analyze/**/*, ./Code/codegen/DriverSwRequest_ert_rtw"
                 }
             }
         }
 
         stage('Deploy') {
+            agent {
+                label 'WinLocalagent' // Label for Windows agent
+            }
             steps {
                 echo "Any deployments of code can be made here"
                 echo "All artifacts of previous stage can be found here"
@@ -80,7 +91,7 @@ pipeline {
             }
             post {
                 always {
-                    archiveArtifacts(artifacts: ["./Design/DriverSwRequest/pipeline/analyze/**/*", "./Code/codegen/DriverSwRequest_ert_rtw"])
+                    archiveArtifacts artifacts: "./Design/DriverSwRequest/pipeline/analyze/**/*, ./Code/codegen/DriverSwRequest_ert_rtw"
                 }
             }
         }
