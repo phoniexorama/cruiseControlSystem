5f578ab8f2b0cdd05b85ff5ad119d2c175d1e557
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-21 09:39:54 +0100

    Update targetSpeedThrottle.groovy

--- targetSpeedThrottle.groovy
+++ targetSpeedThrottle.groovy
@@ -1,75 +1,86 @@
 pipeline {
-    agent {
-        label 'WinLocalagent' //Label for Windows agent
-    }
-
+    agent none
     environment {
-        LOGS_PATH = "./Code"
+        LOGS_PATH = "Code"
         ARTIFACTS_DOWNLOAD_PATH = "C:/Users/${env.GITLAB_USER_LOGIN}/Downloads"
     }
-
     stages {
-        stage('verify') {
+        stage('Verify') {
+            agent {
+                label 'EC2MatlabServer' // Label for Windows agent
+            }
             steps {
                 script {
-                    // This job executes the Model Advisor Checks file for TargetSpeedThrottle model
+                    // This job executes the Model Advisor Check for the model
                     matlabScript("TargetSpeedThrottleModelAdvisor;")
                 }
-                post {
-                    always {
-                        archiveArtifacts(artifacts: ["./Design/TargetSpeedThrottle/pipeline/analyze/**/*", "$LOGS_PATH/logs/"])
-                    }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "$LOGS_PATH/logs/, ./Design/TargetSpeedThrottle/pipeline/analyze/**/*"
                 }
             }
         }
 
-        stage('build') {
+        stage('Build') {
+            agent {
+                label 'LocalMatlabServer' // Label for Windows agent
+            }
             steps {
                 script {
                     // This job performs code generation on the model
                     matlabScript("TargetSpeedThrottleBuild;")
                 }
-                post {
-                    always {
-                        archiveArtifacts(artifacts: ["./Code/codegen/TargetSpeedThrottle_ert_rtw", "./Design/TargetSpeedThrottle/pipeline/analyze/**/*", "$LOGS_PATH/logs/"])
-                    }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "./Code/codegen/TargetSpeedThrottle_ert_rtw, ./Design/TargetSpeedThrottle/pipeline/analyze/**/*, $LOGS_PATH/logs/"
                 }
             }
         }
 
-        stage('testing') {
+        stage('Testing') {
+            agent {
+                label 'EC2MatlabServer' // Label for EC2 agent
+            }
             steps {
                 script {
                     // This job executes the unit tests defined in the collection
                     matlabScript("TargetSpeedThrottleTest;")
                 }
-                post {
-                    always {
-                        archiveArtifacts(artifacts: ["./Design/TargetSpeedThrottle/pipeline/analyze/**/*", "$LOGS_PATH/logs/", "./Code/codegen/TargetSpeedThrottle_ert_rtw"])
-                        junit './Design/TargetSpeedThrottle/pipeline/analyze/testing/TargetSpeedThrottleJUnitFormatTestResults.xml'
-                    }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "./Design/TargetSpeedThrottle/pipeline/analyze/**/*, $LOGS_PATH/logs/, ./Code/codegen/TargetSpeedThrottle_ert_rtw"
+                    junit './Design/TargetSpeedThrottle/pipeline/analyze/testing/TargetSpeedThrottleJUnitFormatTestResults.xml'
                 }
             }
         }
 
-        stage('package') {
+        stage('Package') {
+            agent {
+                label 'LocalMatlabServer' // Label for Windows agent
+            }
             steps {
                 script {
                     // The summary report is generated which shows results from the previous stages.
                     // Any logs that were generated in the previous stages will be cleared after this stage
                     echo "The model TargetSpeedThrottle has been checked"
-                    echo "There is a Summary report generated targetSpeedThrottleReport.html"
+                    echo "There is a Summary report generated TargetSpeedThrottleReport.html"
                     matlabScript("generateXMLFromLogs('TargetSpeedThrottle'); generateHTMLReport('TargetSpeedThrottle'); deleteLogs;")
                 }
-                post {
-                    always {
-                        archiveArtifacts(artifacts: ["./Design/TargetSpeedThrottle/pipeline/analyze/**/*", "./Code/codegen/TargetSpeedThrottle_ert_rtw"])
-                    }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "./Design/TargetSpeedThrottle/pipeline/analyze/**/*, ./Code/codegen/TargetSpeedThrottle_ert_rtw"
                 }
             }
         }
 
         stage('Deploy') {
+            agent {
+                label 'WinLocalagent' // Label for Windows agent
+            }
             steps {
                 echo "Any deployments of code can be made here"
                 echo "All artifacts of previous stage can be found here"
@@ -80,7 +91,7 @@ pipeline {
             }
             post {
                 always {
-                    archiveArtifacts(artifacts: ["./Design/TargetSpeedThrottle/pipeline/analyze/**/*", "./Code/codegen/TargetSpeedThrottle_ert_rtw"])
+                    archiveArtifacts artifacts: "./Design/TargetSpeedThrottle/pipeline/analyze/**/*, ./Code/codegen/TargetSpeedThrottle_ert_rtw"
                 }
             }
         }
