80a03e9d963b58b698493cb1c35fe616ae2cb1cd
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-22 11:34:53 +0100

    Update crs_controller.groovy

--- crs_controller.groovy
+++ crs_controller.groovy
@@ -31,17 +31,19 @@ pipeline {
                 label 'EC2MatlabServer' // Label for Windows agent
             }
             steps {
-                script {
-                    def amazonS3 = new com.amazonaws.services.s3.AmazonS3Client()
-                    amazonS3.setEndpoint("s3.${env.AWS_REGION}.amazonaws.com")
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'your-aws-credentials-id', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    script {
+                        def amazonS3 = new com.amazonaws.services.s3.AmazonS3Client()
+                        amazonS3.setEndpoint("s3.${env.AWS_REGION}.amazonaws.com")
 
-                    // Assuming you have the file to upload in your workspace
-                    def fileContent = readFile(file: "${env.WORKSPACE}/${JOB_NAME}/Code/logs/${env.FILE_NAME}")
-                    def fileObject = new java.io.ByteArrayInputStream(fileContent.bytes)
+                        // Assuming you have the file to upload in your workspace
+                        def fileContent = readFile(file: "${env.WORKSPACE}/${JOB_NAME}/Code/logs/${env.FILE_NAME}")
+                        def fileObject = new java.io.ByteArrayInputStream(fileContent.bytes)
 
-                    amazonS3.putObject(env.BUCKET_NAME, "${JOB_NAME}/Code/Logs/crc_controllerBuildLog.json", fileObject, new com.amazonaws.services.s3.model.ObjectMetadata())
+                        amazonS3.putObject(env.BUCKET_NAME, "${JOB_NAME}/Code/Logs/crc_controllerBuildLog.json", fileObject, new com.amazonaws.services.s3.model.ObjectMetadata())
 
-                    echo "File uploaded successfully to S3 bucket."
+                        echo "File uploaded successfully to S3 bucket."
+                    }
                 }
             }
         }
