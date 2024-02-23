71f569e9dbb992c251c75d485905432c5374ace1
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-23 19:00:43 +0100

    Update crs_controller.groovy

--- crs_controller.groovy
+++ crs_controller.groovy
@@ -29,12 +29,12 @@ pipeline {
                     bat "\"${ZIP_PATH}\" a -tzip \"${ZIP_OUTPUT_PATH}\" \"${BUILD_FOLDER_PATH}\\*\""
 
                     // Set up HTTP request parameters
-                    //def buildUploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.BUILD_ZIP}"
-                    //def folderToUpload = "${ZIP_OUTPUT_PATH}"
+                    def buildUploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.BUILD_ZIP}"
+                    def folderToUpload = "${ZIP_OUTPUT_PATH}"
 
                     // Perform HTTP request to upload the file
-                    //withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                        //sh "curl -u ${USERNAME}:${PASSWORD} -X PUT --data-binary @${folderToUpload} ${buildUploadUrl}"
+                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        sh "curl -u ${USERNAME}:${PASSWORD} -X PUT --data-binary @${folderToUpload} ${buildUploadUrl}"
 
                     //}
 
