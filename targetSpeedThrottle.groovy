434793b2ce7e0621b7f7919217a3785d666d15ff
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-23 21:18:54 +0100

    Update targetSpeedThrottle.groovy

--- targetSpeedThrottle.groovy
+++ targetSpeedThrottle.groovy
@@ -43,7 +43,7 @@ pipeline {
 
                     // Set up HTTP request parameters
                     def buildUploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.BUILD_ZIP}"
-                    def folderToUpload = "Design/DriverSwRequest/pipeline/analyze/${env.BUILD_ZIP}"
+                    def folderToUpload = "Design/TargetSpeedThrottle/pipeline/analyze/${env.BUILD_ZIP}"
 
                     // Perform HTTP request to upload the file
                     withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
