6bac4358e96992025303269a14851f7c38f180ee
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-23 17:51:51 +0100

    Update crs_controller.groovy

--- crs_controller.groovy
+++ crs_controller.groovy
@@ -8,13 +8,53 @@ pipeline {
         WORKSPACE_PATH = "${env.WORKSPACE}"
         PROJECT_NAME = "crs_controller"
         BUILD_ZIP = "build.zip"
-        ANALYZER_PATH = ".\\Design\\${PROJECT_NAME}\\pipeline\\analyze\\build\\"
+        ANALYZER_PATH = ".\\Design\\${PROJECT_NAME}\\pipeline\\analyze\\build"
         ZIP_OUTPUT_PATH = ".\\Design\\${PROJECT_NAME}\\pipeline\\analyze\\${BUILD_ZIP}"
         ARTIFACTORY_URL = 'http://ec2-35-158-218-138.eu-central-1.compute.amazonaws.com:8081/artifactory'
         TARGET_PATH = 'cruisecontrolsystem/crs_controller/'
         MODEL_BUILD_LOG = 'crs_controllerBuildLog.json'
     }
     stages {
+
+
+        stage('Build') {
+            agent {
+                label 'LocalMatlabServer' // Label for Windows agent
+            }
+            steps {
+                script {
+                    // This job performs code generation on the model
+                    matlabScript("crs_controllerBuild;")
+
+                    bat "\"${ZIP_PATH}\" a -tzip \"${ZIP_OUTPUT_PATH}\" \"${ANALYZER_PATH}\""
+
+                    // Set up HTTP request parameters
+                    def buildUploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.BUILD_ZIP}"
+                    def folderToUpload = "${ZIP_OUTPUT_PATH}"
+
+                    // Perform HTTP request to upload the file
+                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        sh "curl -u ${USERNAME}:${PASSWORD} -X PUT --data-binary @${folderToUpload} ${buildUploadUrl}"
+
+                    }
+
+                    // Set up HTTP request parameters
+                    def uploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.MODEL_BUILD_LOG}"
+                    def fileToUpload = "Code/logs/${env.MODEL_BUILD_LOG}"
+
+                    // Perform HTTP request to upload the file
+                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        sh "curl -u ${USERNAME}:${PASSWORD} -X PUT --data-binary @${fileToUpload} ${uploadUrl}"
+
+                    }
+                }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: "./Code/codegen/crs_controller_ert_rtw, ./Design/crs_controller/pipeline/analyze/**/*, $LOGS_PATH/logs/"
+                }
+            }
+        }
 
         stage('Package') {
             agent {
@@ -60,3 +100,4 @@ pipeline {
 def matlabScript(String script) {
     bat "matlab -nodesktop -batch \"openProject('CruiseControlSystem.prj'); ${script}\""
 }
+
