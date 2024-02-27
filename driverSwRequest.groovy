75ceb8e0ba3cabd84636bb20f5937167960fa820
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-27 16:10:27 +0100

    Update driverSwRequest.groovy

--- driverSwRequest.groovy
+++ driverSwRequest.groovy
@@ -3,13 +3,19 @@ pipeline {
     environment {
         LOGS_PATH = "Code"
         ZIP_PATH = "C:\\Program Files\\7-Zip\\7z.exe"
-        BUILD_ZIP = "build.zip"
-        ANALYZER_PATH = ".\\Design\\DriverSwRequest\\pipeline\\analyze\\"
-        ZIP_OUTPUT_PATH = "${env.ANALYZER_PATH}${env.BUILD_ZIP}"
         ARTIFACTORY_URL = 'http://ec2-18-184-131-20.eu-central-1.compute.amazonaws.com:8081/artifactory'
         TARGET_PATH = 'cruisecontrolsystem/DriverSwRequest/'
         MODEL_BUILD_LOG = 'DriverSwRequestBuildLog.json'
+
+        BUILD_ZIP = "build.zip"
+        ANALYZER_PATH = ".\\Design\\DriverSwRequest\\pipeline\\analyze\\"
+        ZIP_OUTPUT_PATH = "${env.ANALYZER_PATH}${env.BUILD_ZIP}"
         BUILD_FOLDER_PATH = ".\\Design\\DriverSwRequest\\pipeline\\analyze\\build\\"
+
+        DRIVER_SW_REQUEST_ERT_RTW_ZIP = "DriverSwRequest_ert_rtw.zip"
+        CODE_GEN_FOLDER_PATH = ".\\Code\\codegen\\"
+        DRIVER_SW_REQUEST_ERT_RTW_PATH = ".\\Code\\codegen\\DriverSwRequest_ert_rtw\\"
+        CODE_GEN_OUTPUT_PATH = "${env.CODE_GEN_FOLDER_PATH}${env.DRIVER_SW_REQUEST_ERT_RTW_ZIP}"
     }
     stages {
         stage('Verify') {
@@ -57,7 +63,18 @@ pipeline {
                     // Perform HTTP request to upload the file
                     withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
                         sh "curl -u ${USERNAME}:${PASSWORD} -X PUT --data-binary @${fileToUpload} ${uploadUrl}"
+                    }
 
+                    // Zip the contents of crs_controller_ert_rtw into crs_controller_ert_rtw.zip
+                    bat "\"${ZIP_PATH}\" a -tzip \"${CODE_GEN_FOLDER_PATH}${DRIVER_SW_REQUEST_ERT_RTW_ZIP}\" \"${DRIVER_SW_REQUEST_ERT_RTW_PATH}*\""
+
+                    // Set up HTTP request parameters for the upload
+                    def ertRtwUploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.DRIVER_SW_REQUEST_ERT_RTW_ZIP}"
+                    def ertRtwFolderToUpload = "${env.CODE_GEN_FOLDER_PATH}${env.DRIVER_SW_REQUEST_ERT_RTW_ZIP}"
+
+                    // Perform HTTP request to upload the zipped folder
+                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        bat "curl -u ${USERNAME}:${PASSWORD} -T ${ertRtwFolderToUpload} ${ertRtwUploadUrl}"
                     }
                 }
             }
@@ -117,6 +134,21 @@ pipeline {
                     bat "del \"${ZIP_OUTPUT_PATH}\""
                     // The summary report is generated which shows results from the previous stages.
                     // Any logs that were generated in the previous stages will be cleared after this stage
+                    // Set up HTTP request parameters
+                    def codeGenDownloadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.DRIVER_SW_REQUEST_ERT_RTW_ZIP}"
+                    def codeGenFolderToDownload = "${CODE_GEN_OUTPUT_PATH}"
+
+                    // Perform HTTP request to upload the file
+                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        bat "curl -u %USERNAME%:%PASSWORD% -o ${codeGenFolderToDownload} ${codeGenDownloadUrl}"
+                    }
+
+                    // Unzip the build.zip file
+                    bat "\"${ZIP_PATH}\" x \"${CODE_GEN_OUTPUT_PATH}\" -o\"${DRIVER_SW_REQUEST_ERT_RTW_PATH}\""
+
+                    // Delete the build.zip file after extraction
+                    bat "del \"${CODE_GEN_OUTPUT_PATH}\""
+
                     echo "The model DriverSwRequest has been checked"
                     echo "There is a Summary report generated DriverSwRequestSummaryReport.html which is present in analyze folder"
                     matlabScript("generateXMLFromLogs('DriverSwRequest'); generateHTMLReport('DriverSwRequest'); deleteLogs;")
