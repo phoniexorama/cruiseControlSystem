31f6409507e5647dbdbf21c04d5e8eaf665fb878
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-27 17:26:16 +0100

    Update targetSpeedThrottle.groovy

--- targetSpeedThrottle.groovy
+++ targetSpeedThrottle.groovy
@@ -3,13 +3,19 @@ pipeline {
     environment {
         LOGS_PATH = "Code"
         ZIP_PATH = "C:\\Program Files\\7-Zip\\7z.exe"
-        BUILD_ZIP = "build.zip"
-        ANALYZER_PATH = ".\\Design\\TargetSpeedThrottle\\pipeline\\analyze\\"
-        ZIP_OUTPUT_PATH = "${env.ANALYZER_PATH}${env.BUILD_ZIP}"
         ARTIFACTORY_URL = 'http://ec2-18-184-131-20.eu-central-1.compute.amazonaws.com:8081/artifactory'
         TARGET_PATH = 'cruisecontrolsystem/TargetSpeedThrottle/'
         MODEL_BUILD_LOG = 'TargetSpeedThrottleBuildLog.json'
+
+        BUILD_ZIP = "build.zip"
+        ANALYZER_PATH = ".\\Design\\TargetSpeedThrottle\\pipeline\\analyze\\"
+        ZIP_OUTPUT_PATH = "${env.ANALYZER_PATH}${env.BUILD_ZIP}"
         BUILD_FOLDER_PATH = ".\\Design\\TargetSpeedThrottle\\pipeline\\analyze\\build\\"
+
+        TARGET_SPEED_THROTTLE_ERT_RTW_ZIP = "TargetSpeedThrottle_ert_rtw.zip"
+        CODE_GEN_FOLDER_PATH = ".\\Code\\codegen\\"
+        TARGET_SPEED_THROTTLE_ERT_RTW_PATH = ".\\Code\\codegen\\TargetSpeedThrottle_ert_rtw\\"
+        CODE_GEN_OUTPUT_PATH = "${env.CODE_GEN_FOLDER_PATH}${env.TARGET_SPEED_THROTTLE_ERT_RTW_ZIP}"
     }
     stages {
         stage('Verify') {
@@ -57,7 +63,18 @@ pipeline {
                     // Perform HTTP request to upload the file
                     withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
                         sh "curl -u ${USERNAME}:${PASSWORD} -X PUT --data-binary @${fileToUpload} ${uploadUrl}"
+                    }
 
+                    // Zip the contents of crs_controller_ert_rtw into crs_controller_ert_rtw.zip
+                    bat "\"${ZIP_PATH}\" a -tzip \"${CODE_GEN_FOLDER_PATH}${TARGET_SPEED_THROTTLE_ERT_RTW_ZIP}\" \"${TARGET_SPEED_THROTTLE_ERT_RTW_PATH}*\""
+
+                    // Set up HTTP request parameters for the upload
+                    def ertRtwUploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.TARGET_SPEED_THROTTLE_ERT_RTW_ZIP}"
+                    def ertRtwFolderToUpload = "${env.CODE_GEN_FOLDER_PATH}${env.TARGET_SPEED_THROTTLE_ERT_RTW_ZIP}"
+
+                    // Perform HTTP request to upload the zipped folder
+                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        bat "curl -u ${USERNAME}:${PASSWORD} -T ${ertRtwFolderToUpload} ${ertRtwUploadUrl}"
                     }
                 }
             }
@@ -115,6 +132,21 @@ pipeline {
 
                     // Delete the build.zip file after extraction
                     bat "del \"${ZIP_OUTPUT_PATH}\""
+
+                    // Set up HTTP request parameters
+                    def codeGenDownloadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.TARGET_SPEED_THROTTLE_ERT_RTW_ZIP}"
+                    def codeGenFolderToDownload = "${CODE_GEN_OUTPUT_PATH}"
+
+                    // Perform HTTP request to upload the file
+                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        bat "curl -u %USERNAME%:%PASSWORD% -o ${codeGenFolderToDownload} ${codeGenDownloadUrl}"
+                    }
+
+                    // Unzip the build.zip file
+                    bat "\"${ZIP_PATH}\" x \"${CODE_GEN_OUTPUT_PATH}\" -o\"${TARGET_SPEED_THROTTLE_ERT_RTW_PATH}\""
+
+                    // Delete the build.zip file after extraction
+                    bat "del \"${CODE_GEN_OUTPUT_PATH}\""
                     // The summary report is generated which shows results from the previous stages.
                     // Any logs that were generated in the previous stages will be cleared after this stage
                     echo "The model TargetSpeedThrottle has been checked"
