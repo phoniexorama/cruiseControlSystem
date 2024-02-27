8fa26f09881f48d97782fa924f9b8ba6cf9776bf
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-27 14:24:30 +0100

    Update crs_controller.groovy

--- crs_controller.groovy
+++ crs_controller.groovy
@@ -2,21 +2,25 @@ pipeline {
     agent none
 
     environment {
+
         LOGS_PATH = "Code"
-        ZIP_PATH = "\"C:\\Program Files\\7-Zip\\7z.exe\"" // Enclose the path in double quotes
-        BUILD_ZIP = "build.zip"
-        ANALYZER_PATH = ".\\Design\\crs_controller\\pipeline\\analyze\\"
+        ZIP_PATH = "C:\\Program Files\\7-Zip\\7z.exe"
         ARTIFACTORY_URL = 'http://ec2-35-159-25-238.eu-central-1.compute.amazonaws.com:8081/artifactory'
         TARGET_PATH = 'cruisecontrolsystem/crs_controller/'
         MODEL_BUILD_LOG = 'crs_controllerBuildLog.json'
+
+        BUILD_ZIP = "build.zip"
+        ANALYZER_PATH = ".\\Design\\crs_controller\\pipeline\\analyze\\"
         BUILD_FOLDER_PATH = ".\\Design\\crs_controller\\pipeline\\analyze\\build\\"
+        ZIP_OUTPUT_PATH = "${env.ANALYZER_PATH}${env.BUILD_ZIP}"
+
         CRS_CONTROLLER_ERT_RTW_ZIP = "crs_controller_ert_rtw.zip"
         CODE_GEN_FOLDER_PATH = "Code\\codegen\\"
-        CRS_CONTROLLER_ERT_RTW_PATH = "Code\\codegen\\crs_controller_ert_rtw\\" // Path to the folder
+        CRS_CONTROLLER_ERT_RTW_PATH = "Code\\codegen\\crs_controller_ert_rtw\\"
         CODE_GEN_OUTPUT_PATH = "${env.CODE_GEN_FOLDER_PATH}${env.CRS_CONTROLLER_ERT_RTW_ZIP}"
     }
-
     stages {
+
         stage('Verify') {
             agent {
                 label 'EC2MatlabServer' // Label for Windows agent
@@ -26,16 +30,19 @@ pipeline {
                     // This job executes the Model Advisor Check for the model
                     matlabScript("crs_controllerModelAdvisor;")
 
+
                     // Zip the contents of crs_controller_ert_rtw into crs_controller_ert_rtw.zip
-                    bat "${ZIP_PATH} a -tzip ${CODE_GEN_OUTPUT_PATH} ${CRS_CONTROLLER_ERT_RTW_PATH}* -r" // Use -r to include contents recursively
+                    bat "\"${ZIP_PATH}\" a -tzip \"${CODE_GEN_FOLDER_PATH}${CRS_CONTROLLER_ERT_RTW_ZIP}\" \"${CRS_CONTROLLER_ERT_RTW_PATH}*\""
 
                     // Set up HTTP request parameters for the upload
-                    def ertRtwUploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.CRS_CONTROLLER_ERT_RTW_ZIP}"
+                    def ertRtwUploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${CRS_CONTROLLER_ERT_RTW_ZIP}"
+                    def ertRtwFolderToUpload = "${CODE_GEN_FOLDER_PATH}${CRS_CONTROLLER_ERT_RTW_ZIP}"
 
-                    // Perform HTTP request to upload the file
+                    // Perform HTTP request to upload the zipped folder
                     withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                        sh "curl -u ${USERNAME}:${PASSWORD} -T ${CODE_GEN_OUTPUT_PATH} ${ertRtwUploadUrl}"
+                        sh "curl -u ${USERNAME}:${PASSWORD} -T ${ertRtwFolderToUpload} ${ertRtwUploadUrl}"
                     }
+                
                 }
             }
             post {
