9d7317353dcf120d4c733f97b7189a778f1df375
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-27 18:23:27 +0100

    Update crs_controller.groovy

--- crs_controller.groovy
+++ crs_controller.groovy
@@ -21,22 +21,7 @@ pipeline {
     }
     stages {
 
-        stage('Verify') {
-            agent {
-                label 'EC2MatlabServer' // Label for Windows agent
-            }
-            steps {
-                script {
-                    // This job executes the Model Advisor Check for the model
-                    matlabScript("crs_controllerModelAdvisor;")
-                }
-            }
-            post {
-                always {
-                    archiveArtifacts artifacts: "$LOGS_PATH/logs/, ./Design/crs_controller/pipeline/analyze/**/*"
-                }
-            }
-        }
+
 
         stage('Build') {
             agent {
@@ -88,119 +73,7 @@ pipeline {
             }
         }
 
-        stage('Testing') {
-            agent {
-                label 'EC2MatlabServer' // Label for EC2 agent
-            }
-            steps {
-                script {
-                    // This job executes the unit tests defined in the collection
-                    matlabScript("crs_controllerTestFile;")
-                }
-            }
-            post {
-                always {
-                    archiveArtifacts artifacts: "Design/crs_controller/pipeline/analyze/**/*, $LOGS_PATH/logs/, Code/codegen/crs_controller_ert_rtw"
-                    //junit 'Design/crs_controller/pipeline/analyze/testing/crs_controllerJUnitFormatTestResults.xml'
-                }
-            }
-        }
 
-        stage('Package') {
-            agent {
-                label 'EC2MatlabServer' // Assuming you have a label for Windows agents
-            }
-            steps {
-                script {
-                    // Set up HTTP request parameters
-                    def downloadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.MODEL_BUILD_LOG}"
-                    def fileToDownload = "Code/logs/${env.MODEL_BUILD_LOG}"
-
-                    // Perform HTTP request to download the file
-                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                        bat "curl -u %USERNAME%:%PASSWORD% -o ${fileToDownload} ${downloadUrl}"
-                    }
-
-                    // Set up HTTP request parameters
-                    def buildDownloadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.BUILD_ZIP}"
-                    def folderToDownload = "${ZIP_OUTPUT_PATH}"
-
-                    // Perform HTTP request to upload the file
-                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                        bat "curl -u %USERNAME%:%PASSWORD% -o ${folderToDownload} ${buildDownloadUrl}"
-                    }
-
-                    // Unzip the build.zip file
-                    bat "\"${ZIP_PATH}\" x \"${ZIP_OUTPUT_PATH}\" -o\"${BUILD_FOLDER_PATH}\""
-
-                    // Delete the build.zip file after extraction
-                    bat "del \"${ZIP_OUTPUT_PATH}\""
-
-                    // Set up HTTP request parameters
-                    def codeGenDownloadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.CRS_CONTROLLER_ERT_RTW_ZIP}"
-                    def codeGenFolderToDownload = "${CODE_GEN_OUTPUT_PATH}"
-
-                    // Perform HTTP request to upload the file
-                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                        bat "curl -u %USERNAME%:%PASSWORD% -o ${codeGenFolderToDownload} ${codeGenDownloadUrl}"
-                    }
-
-                    // Unzip the build.zip file
-                    bat "\"${ZIP_PATH}\" x \"${CODE_GEN_OUTPUT_PATH}\" -o\"${CRS_CONTROLLER_ERT_RTW_PATH}\""
-
-                    // Delete the build.zip file after extraction
-                    bat "del \"${CODE_GEN_OUTPUT_PATH}\""
-
-                    echo "The model crs_controller has been checked"
-                    echo "There is a Summary report generated crs_controllerReport.html"
-                    // The summary report is generated which shows results from the previous stages.
-                    // Any logs that were generated in the previous stages will be cleared after this stage
-                    matlabScript("generateXMLFromLogs('crs_controller'); generateHTMLReport('crs_controller'); deleteLogs;")
-
-                }
-            }
-            post {
-                always {
-                    archiveArtifacts artifacts: "Design/crs_controller/pipeline/analyze/**/*, Code/codegen/crs_controller_ert_rtw"
-                }
-            }
-        }
-
-        stage('Deploy') {
-            agent {
-                label 'EC2MatlabServer' // Label for Windows agent
-            }
-            steps {
-                script {
-                    echo "Any deployments of code can be made here"
-                    echo "All artifacts of previous stage can be found here"
-                    // Curl command to download artifacts
-                    //bat "curl.exe --location --output \"$ARTIFACTS_DOWNLOAD_PATH/Crs_ControllerArtifacts.zip\" --header \"PRIVATE-TOKEN: %CIPROJECTTOKEN%\" \"%CI_SERVER_URL%/api/v4/projects/%CI_PROJECT_ID%/jobs/artifacts/%CI_COMMIT_BRANCH%/download?job=Crs_ControllerPackage\""
-                    publishHTML([
-                            allowMissing: false,
-                            alwaysLinkToLastBuild: false,
-                            keepAll: true,
-                            reportDir: 'Design/crs_controller/pipeline/analyze/verify/',
-                            reportFiles: 'crs_controllerModelAdvisorReport.html',
-                            reportName: 'Model Advisor Report'
-                    ])
-
-                    publishHTML([
-                            allowMissing: false,
-                            alwaysLinkToLastBuild: false,
-                            keepAll: true,
-                            reportDir: 'Design/crs_controller/pipeline/analyze/package/',
-                            reportFiles: 'crs_controllerSummaryReport.html',
-                            reportName: 'Summary Report'
-                    ])
-                }
-            }
-            post {
-                always {
-                    archiveArtifacts artifacts: "Design/crs_controller/pipeline/analyze/**/*, Code/codegen/crs_controller_ert_rtw"
-                }
-            }
-        }
 
     }
 }
