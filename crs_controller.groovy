454e4c033bd199d405c72cf2d3853e48e7cb457f
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-02-27 13:21:32 +0100

    Update crs_controller.groovy

--- crs_controller.groovy
+++ crs_controller.groovy
@@ -12,6 +12,11 @@ pipeline {
         TARGET_PATH = 'cruisecontrolsystem/crs_controller/'
         MODEL_BUILD_LOG = 'crs_controllerBuildLog.json'
         BUILD_FOLDER_PATH = ".\\Design\\crs_controller\\pipeline\\analyze\\build\\"
+
+        CRS_CONTROLLER_ERT_RTW_ZIP = "crs_controller_ert_rtw.zip"
+        CODE_GEN_FOLDER_PATH = "Code\\codegen\\"
+        CRS_CONTROLLER_ERT_RTW_PATH = "Code\\codegen\\crs_controller_ert_rtw\\"
+        CODE_GEN_OUTPUT_PATH = "${env.CODE_GEN_FOLDER_PATH}${env.CRS_CONTROLLER_ERT_RTW_ZIP}"
     }
     stages {
 
@@ -23,6 +28,18 @@ pipeline {
                 script {
                     // This job executes the Model Advisor Check for the model
                     matlabScript("crs_controllerModelAdvisor;")
+
+                    // Zip the contents of crs_controller_ert_rtw into crs_controller_ert_rtw.zip
+                    bat "\"${ZIP_PATH}\" a -tzip \"${CODE_GEN_OUTPUT_PATH}\" \"${CRS_CONTROLLER_ERT_RTW_PATH}\\*\""
+
+                    // Set up HTTP request parameters for the second upload
+                    def ertRtwUploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.CRS_CONTROLLER_ERT_RTW_ZIP}"
+                    def ertRtwFolderToUpload = "${env.CODE_GEN_FOLDER_PATH}${env.CRS_CONTROLLER_ERT_RTW_ZIP}"
+
+                    // Perform HTTP request to upload the second file
+                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        sh "curl -u ${USERNAME}:${PASSWORD} -X PUT --data-binary @${ertRtwFolderToUpload} ${ertRtwUploadUrl}"
+                    }
                 }
             }
             post {
@@ -31,128 +48,6 @@ pipeline {
                 }
             }
         }
-
-        stage('Build') {
-            agent {
-                label 'LocalMatlabServer' // Label for Windows agent
-            }
-            steps {
-                script {
-                    // This job performs code generation on the model
-                    matlabScript("crs_controllerBuild;")
-
-                    //bat "\"${ZIP_PATH}\" a -tzip \"${ZIP_OUTPUT_PATH}\" \"${ANALYZER_PATH}\""
-                    bat "\"${ZIP_PATH}\" a -tzip \"${ZIP_OUTPUT_PATH}\" \"${BUILD_FOLDER_PATH}\\*\""
-
-                    // Set up HTTP request parameters
-                    def buildUploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.BUILD_ZIP}"
-                    def folderToUpload = "Design/crs_controller/pipeline/analyze/${env.BUILD_ZIP}"
-
-                    // Perform HTTP request to upload the file
-                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                        sh "curl -u ${USERNAME}:${PASSWORD} -X PUT --data-binary @${folderToUpload} ${buildUploadUrl}"
-                    }
-
-                    // Set up HTTP request parameters
-                    def uploadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.MODEL_BUILD_LOG}"
-                    def fileToUpload = "Code/logs/${env.MODEL_BUILD_LOG}"
-
-                    // Perform HTTP request to upload the file
-                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                        sh "curl -u ${USERNAME}:${PASSWORD} -X PUT --data-binary @${fileToUpload} ${uploadUrl}"
-
-                    }
-                }
-            }
-            post {
-                always {
-                    archiveArtifacts artifacts: "./Code/codegen/crs_controller_ert_rtw, ./Design/crs_controller/pipeline/analyze/**/*, $LOGS_PATH/logs/"
-                }
-            }
-        }
-
-        stage('Testing') {
-            agent {
-                label 'EC2MatlabServer' // Label for EC2 agent
-            }
-            steps {
-                script {
-                    // This job executes the unit tests defined in the collection
-                    matlabScript("crs_controllerTestFile;")
-                }
-            }
-            post {
-                always {
-                    archiveArtifacts artifacts: "./Design/crs_controller/pipeline/analyze/**/*, $LOGS_PATH/logs/, ./Code/codegen/crs_controller_ert_rtw"
-                    //junit 'Design/crs_controller/pipeline/analyze/testing/crs_controllerJUnitFormatTestResults.xml'
-                }
-            }
-        }
-
-        stage('Package') {
-            agent {
-                label 'EC2MatlabServer' // Assuming you have a label for Windows agents
-            }
-            steps {
-                script {
-                    // Set up HTTP request parameters
-                    def downloadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.MODEL_BUILD_LOG}"
-                    def fileToDownload = "Code/logs/${env.MODEL_BUILD_LOG}"
-
-                    // Perform HTTP request to download the file
-                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                        bat "curl -u %USERNAME%:%PASSWORD% -o ${fileToDownload} ${downloadUrl}"
-                    }
-
-                    // Set up HTTP request parameters
-                    def buildDownloadUrl = "${env.ARTIFACTORY_URL}/${env.TARGET_PATH}/${env.BUILD_ZIP}"
-                    def folderToDownload = "${ZIP_OUTPUT_PATH}"
-
-                    // Perform HTTP request to upload the file
-                    withCredentials([usernamePassword(credentialsId: 'artifactory_credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                        bat "curl -u %USERNAME%:%PASSWORD% -o ${folderToDownload} ${buildDownloadUrl}"
-                    }
-
-                    // Unzip the build.zip file
-                    bat "\"${ZIP_PATH}\" x \"${ZIP_OUTPUT_PATH}\" -o\"${BUILD_FOLDER_PATH}\""
-
-                    // Delete the build.zip file after extraction
-                    bat "del \"${ZIP_OUTPUT_PATH}\""
-
-                    echo "The model crs_controller has been checked"
-                    echo "There is a Summary report generated crs_controllerReport.html"
-                    // The summary report is generated which shows results from the previous stages.
-                    // Any logs that were generated in the previous stages will be cleared after this stage
-                    matlabScript("generateXMLFromLogs('crs_controller'); generateHTMLReport('crs_controller'); deleteLogs;")
-
-                }
-            }
-            post {
-                always {
-                    archiveArtifacts artifacts: "Design/crs_controller/pipeline/analyze/**/*, ./Code/codegen/crs_controller_ert_rtw"
-                }
-            }
-        }
-
-        stage('Deploy') {
-            agent {
-                label 'EC2MatlabServer' // Label for Windows agent
-            }
-            steps {
-                script {
-                    echo "Any deployments of code can be made here"
-                    echo "All artifacts of previous stage can be found here"
-                    // Curl command to download artifacts
-                    //bat "curl.exe --location --output \"$ARTIFACTS_DOWNLOAD_PATH/Crs_ControllerArtifacts.zip\" --header \"PRIVATE-TOKEN: %CIPROJECTTOKEN%\" \"%CI_SERVER_URL%/api/v4/projects/%CI_PROJECT_ID%/jobs/artifacts/%CI_COMMIT_BRANCH%/download?job=Crs_ControllerPackage\""
-                }
-            }
-            post {
-                always {
-                    archiveArtifacts artifacts: "Design/crs_controller/pipeline/analyze/**/*, ./Code/codegen/crs_controller_ert_rtw"
-                }
-            }
-        }
-
     }
 }
 
